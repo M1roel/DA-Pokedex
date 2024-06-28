c1c6fa463dc4572277db29091f98bcd00b54d08c
M1roel <157500795+M1roel@users.noreply.github.com>
2024-06-28 13:58:48 +0200

    add API functions to script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,38 @@
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=10";
+
 function init() {
-    
+  fetchDataJson();
 }
 
+async function fetchDataJson() {
+  let response = await fetch(BASE_URL);
+  data = await response.json();
+  showPokemon(data.results);
+}
+
+async function showPokemon(pokemons) {
+  document.getElementById("content").innerHTML = "";
+
+  for (let i = 0; i < pokemons.length; i++) {
+    const pokemon = pokemons[i];
+    let pokemonDetails = await fetchPokemonDetails(pokemon.url);
+    document.getElementById("content").innerHTML +=
+      generatePokemon(pokemonDetails);
+  }
+}
+
+async function fetchPokemonDetails(url) {
+  let response = await fetch(url);
+  return await response.json();
+}
+
+function generatePokemon(pokemon) {
+  return `
+      <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+        <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+      `;
+}
