8f43109e2d1e377f117194b7250775a111e27db5
M1roel <ppfautsch@yahoo.de>
2024-06-30 17:49:13 +0200

    add progress bars

--- src/js/script.js
+++ src/js/script.js
@@ -30,12 +30,25 @@ async function fetchPokemonDetails(url) {
 
 function generatePokemon(pokemon) {
   const capitalizedPokemonName = capitalizeFirstLetter(pokemon.name);
+  let statsHtml = '';
+
+  for (const stat of pokemon.stats) {
+    const statName = capitalizeFirstLetter(stat.stat.name.replace('-', ' '));
+    const statValue = stat.base_stat;
+    statsHtml += `
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" style="width: ${statValue}%" aria-valuenow="${statValue}" aria-valuemin="0" aria-valuemax="100">${statName}: ${statValue}</div>
+      </div>
+    `;
+  }
+
   return `
     <div class="card" style="width: 18rem;">
       <div class="pkmName">${capitalizedPokemonName}</div>
       <img class="card-img-top" src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
       <div class="card-body">
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        ${statsHtml}
       </div>
     </div>
   `;
